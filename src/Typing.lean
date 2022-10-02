1ec5f65634c48271ac7c5eebfbbf425031abe3cf
riib11 <henry@monkeyface.com>
2022-10-02 10:26:24 -0400

    renaming

--- src/Typing.lean
+++ src/Typing.lean
@@ -22,70 +22,70 @@ def Ctx.consTyp (x : Name) (α : Typ) (Γ : Ctx) : Ctx where
 
 -- Kinding
 
-def inferKnd (Γ : Ctx) (α : Typ) : Option Knd :=
+def Knd.infer (Γ : Ctx) (α : Typ) : Option Knd :=
   match α with 
   | Typ.bas basTyp => 
     match basTyp with 
     | BasTyp.unt => Knd.unt
   | Typ.var x => Γ.knds x
   | Typ.all x κ β => do
-    let μ <- inferKnd (Ctx.consKnd x κ Γ) β
+    let μ <- Knd.infer (Ctx.consKnd x κ Γ) β
     return Knd.arr κ μ
   | Typ.arr .. => Knd.unt
 
-def checkKnd (Γ : Ctx) (α : Typ) (κ : Knd) : Bool :=
-  match inferKnd Γ α with 
+def Knd.check (Γ : Ctx) (α : Typ) (κ : Knd) : Bool :=
+  match Knd.infer Γ α with 
   | some κ' => κ == κ' 
   | none => false
 
 -- Typing
 
-def subTyp (x : Name) (ξ : Typ) (α : Typ) : Typ :=
+def Typ.sub (x : Name) (ξ : Typ) (α : Typ) : Typ :=
   match α with 
   | Typ.bas .. => α
   | Typ.var y => if x == y then ξ else α
-  | Typ.arr β γ => Typ.arr (subTyp x ξ β) (subTyp x ξ γ)
-  | Typ.all y κ β => if x == y then α else Typ.all y κ (subTyp x ξ β)   
+  | Typ.arr β γ => Typ.arr (Typ.sub x ξ β) (Typ.sub x ξ γ)
+  | Typ.all y κ β => if x == y then α else Typ.all y κ (Typ.sub x ξ β)   
 
-def inferTyp (Γ : Ctx) (t : Trm) : Option Typ :=
+def Typ.infer (Γ : Ctx) (t : Trm) : Option Typ :=
   match t with 
   | Trm.bas t' => 
     match t' with 
     | BasTrm.unt => Typ.bas BasTyp.unt
   | Trm.var x => Γ.typs x
   | Trm.lam x α b => do
-    let β <- inferTyp (Ctx.consTyp x α Γ) b
+    let β <- Typ.infer (Ctx.consTyp x α Γ) b
     return Typ.arr α β
   | Trm.all x κ b => do
-    let β <- inferTyp (Ctx.consKnd x κ Γ) b
+    let β <- Typ.infer (Ctx.consKnd x κ Γ) b
     return Typ.all x κ β 
   | Trm.app f a => do
-    let φ <- inferTyp Γ f
+    let φ <- Typ.infer Γ f
     match φ with 
     | Typ.arr α β => do
-      let α' <- inferTyp Γ a
+      let α' <- Typ.infer Γ a
       guard $ α == α'
       return β
     | _ => none
   | Trm.ins f α => do
-    let φ <- inferTyp Γ f
+    let φ <- Typ.infer Γ f
     match φ with
     | Typ.all x κ β => do
-      let κ' <- inferKnd Γ α
+      let κ' <- Knd.infer Γ α
       guard $ κ == κ'
-      return subTyp x α β
+      return Typ.sub x α β
     | _ => none
   | Trm.loc x β b a => do
-    let β' <- inferTyp Γ b 
+    let β' <- Typ.infer Γ b 
     guard (β == β')
-    inferTyp (Ctx.consTyp x β Γ) a
+    Typ.infer (Ctx.consTyp x β Γ) a
 
-def checkTyp (Γ : Ctx) (t : Trm) (α : Typ) : Bool :=
-  match inferTyp Γ t with 
+def Typ.check (Γ : Ctx) (t : Trm) (α : Typ) : Bool :=
+  match Typ.infer Γ t with 
   | some α' => α == α'
   | none => false
 
 -- #eval 
---   inferTyp 
+--   Typ.infer 
 --     (Ctx.consTyp (Name.mk "x") (Typ.bas BasTyp.unt) Ctx.nil)
 --     (Trm.var (Name.mk "x")) 
